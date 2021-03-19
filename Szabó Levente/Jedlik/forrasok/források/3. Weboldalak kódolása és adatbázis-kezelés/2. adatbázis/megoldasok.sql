c2619d578c85cb39b4c54760d3aba6318fd5c7b6
Szabó Levente <levente.szabo@katonaj-mkszig.hu>
2021-03-19 21:12:14 +0100

    Kész

--- Szabó Levente/Jedlik/forrasok/források/3. Weboldalak kódolása és adatbázis-kezelés/2. adatbázis/megoldasok.sql
+++ Szabó Levente/Jedlik/forrasok/források/3. Weboldalak kódolása és adatbázis-kezelés/2. adatbázis/megoldasok.sql
@@ -2,33 +2,35 @@
 
 
 -- 1. feladat:
-CREATE DATABASE formula1 
-CHARACTER SET utf8 
+CREATE DATABASE formula1
+DEFAULT CHARSET=utf8
 COLLATE utf8_hungarian_ci;
 
 -- 3. feladat:
 UPDATE nagydijak
-SET korokszama="70"
-WHERE nev="Hungarian Grand Prix";
+SET korokszama=70
+WHERE nev LIKE"Hungarian%";
 
 -- 4. feladat:
-SELECT vezeteknev, rajtszam, csapatnev, 2019-SUBSTRING(szuletesidatum, 1,4) AS eletkor FROM `pilotak` ORDER BY eletkor DESC
+SELECT vezeteknev, rajtszam,  csapatnev, 
+2019-year(szuletesidatum) AS eletkor FROM pilotak
+ORDER BY eletkor DESC;
 
 -- 5. feladat:
-SELECT nev, vezeteknev, keresztnev, versenynap FROM `eredmenyek` 
-INNER JOIN pilotak
-ON pilotak.id = eredmenyek.pilotaid
-INNER JOIN nagydijak
-ON nagydijak.id = eredmenyek.nagydijid
-WHERE helyezes = "1"
+SELECT nev, vezeteknev, keresztnev, versenynap FROM eredmenyek
+JOIN nagydijak
+ON eredmenyek.nagydijid=nagydijak.id
+JOIN pilotak
+ON pilotak.id=eredmenyek.pilotaid
+WHERE helyezes = 1
 ORDER BY versenynap ASC;
 
 -- 6. feladat:
-SELECT concat(keresztnev, ' ', vezeteknev) AS nev, csapatnev, SUM(pontszam) AS osszpontszam FROM `eredmenyek` 
-INNER JOIN pilotak 
-ON pilotak.id = eredmenyek.pilotaid 
-INNER JOIN nagydijak 
-ON nagydijak.id = eredmenyek.nagydijid
+SELECT concat(keresztnev, ' ', vezeteknev) AS nev, 
+csapatnev, SUM(pontszam) AS osszpontszam
+FROM `eredmenyek` 
+JOIN pilotak
+ON pilotak.id=eredmenyek.pilotaid
 GROUP BY vezeteknev, keresztnev, csapatnev
 ORDER BY osszpontszam DESC
-LIMIT 3;
+LIMIT 3
